bb8dac0e16c8028632d0d01172580cd30e6cf8dc
Ksenia <example@gmail.com>
2021-12-13 11:58:55 +0100

    first commit

---
+++ PREVIEW/js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+
+// 2 exemple
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Ksenia";
+
+//alert(`salut, ${user}`);
+
+// 3 exemple
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++; прибавляет к значению 1
+// decr--; вычитает из значения 1. Можно использовать такую запись и в строках ниже, но имеет значение до (изменяется) или после (не изменяется)
+
+console.log(incr);
+console.log(decr);
+
+console.log(5 % 2); //равно 1, показывает остаток после деления на 2
+
+console.log(2 * 4 == 8);  // для сравнения значений 2 знака равенства, для сравнения типов данных - 3. 
