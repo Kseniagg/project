754e52a587eb2998f6bbf59012ea0eb8480a87a8
Ksenia <example@gmail.com>
2022-02-21 17:13:05 +0100

    21/02/2022

--- PREVIEW/js/script.js
+++ PREVIEW/js/script.js
@@ -1,45 +1,119 @@
-"use strict";
+'use strict';
 
-//alert('Hello');
+/*if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}*/
 
-//const result = confirm("Are you here?");
-//console.log(result);
+//const num = 50;//
 
-//const answer = +prompt("Вам есть 18?", "18");
-//console.log(answer + 5);
+/*if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Beaucoup');
+} else {//
+    console.log('OK!');
+}*/
 
-//const answers = [];
+//(num === 50) ? console.log('Ok!') : console.log('Error');//
 
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
+/*const num = 50;
 
-//console.log(typeof(answers));
+switch (num) {
+    case 49:
+        console.log('Neverno');
+        break;
+    case 100:
+       console.log('Neverno');
+        break;
+    case 50:
+        console.log('verno');
+        break;
+    default:
+        console.log('Ne v etot raz'); //clg
+        break;
+} */
 
-// 2 exemple
 
-//const category = 'toys';
+let numberOfFilms;
 
-//console.log(`https://someurl.com/${category}/5`);
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const user = "Ksenia";
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
-//alert(`salut, ${user}`);
+start();
 
-// 3 exemple
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log('arr' + " - object");
-console.log(4 + "5");
+//const a = prompt('Один из последних просмотренных фильмов?', ''),
+// b = prompt('На сколько его оцените?', ''),
+//c = prompt('Один из последних просмотренных фильмов?', ''),
+// d = prompt('На сколько его оцените?', '');
 
-let incr = 10,
-    decr = 10;
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
 
-// incr++; прибавляет к значению 1
-// decr--; вычитает из значения 1. Можно использовать такую запись и в строках ниже, но имеет значение до (изменяется) или после (не изменяется)
 
-console.log(incr);
-console.log(decr);
 
-console.log(5 % 2); //равно 1, показывает остаток после деления на 2
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько его оцените?', '');
 
-console.log(2 * 4 == 8);  // для сравнения значений 2 знака равенства, для сравнения типов данных - 3. 
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;  // база данных
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;  // вернуть на один цикл назад (если условие не выполнилось)
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка'); // если ни одно условие не выполнилось
+    }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+
+
